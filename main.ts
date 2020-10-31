2276c863eedab7c0e0b9753cc5ddf1135c0d98b0
Yu410902038 <71751927+Yu410902038@users.noreply.github.com>
2020-10-31 18:19:01 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,4 +6,11 @@ basic.forever(function () {
         basic.pause(500)
     }
     basic.clearScreen()
+    for (let i = 0; i <= 8; i++) {
+        for (let j = 0; j <= 4; j++) {
+            led.plot(j, i - j)
+        }
+        basic.pause(500)
+    }
+    basic.clearScreen()
 })
