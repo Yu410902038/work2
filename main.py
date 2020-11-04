8e2da6f5e476ed8a54dee568d52e08eefcc367e1
Yu410902038 <71751927+Yu410902038@users.noreply.github.com>
2020-11-04 16:01:24 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def on_forever():
-    for x in range(1, 4):
-        for y in range(4):
-            basic.pause(200)
-            led.plot(x, y)
-basic.forever(on_forever)
+basic.forever(function () {
+	
+})
--- main.ts
+++ main.ts
@@ -1,14 +1,8 @@
 basic.forever(function () {
-    for (let i = 0; i <= 8; i++) {
-        for (let j = 0; j <= 4; j++) {
-            led.plot(4 - j, 4 - (i - j))
-        }
-        basic.pause(500)
-    }
-    basic.clearScreen()
-    for (let i = 0; i <= 8; i++) {
-        for (let j = 0; j <= 4; j++) {
-            led.plot(j, i - j)
+    for (let i = 0; i <= 4; i++) {
+        for (let j = 0; j <= i; j++) {
+            led.plot(4 - j, i)
+            led.plot(4 - i, j)
         }
         basic.pause(500)
     }
